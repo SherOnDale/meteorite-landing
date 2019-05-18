02aec8fe718ad608a0a9402cad5ddc879f5dfa6d
Sherin Binu <sherinbinu@hotmail.com>
2019-05-18 20:36:29 +0530

    add api response status

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="table__container">
-    <div class="table">
+    <div class="table" v-if="responseLoaded">
       <table-row header/>
       <table-row v-for="item in filteredData" :data="item" :key="item.id"/>
     </div>
+    <div class="status" v-else>{{statusMessage}}</div>
   </div>
 </template>
 
 <script>
 import TableRow from "./TableRow";
+import { setTimeout } from "timers";
 export default {
   name: "app-table",
   components: {
@@ -21,21 +23,39 @@ export default {
     }
   },
   mounted() {
-    fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
+    this.fetchWithTimeout("https://data.nasa.gov/resource/gh4g-9sfh.json")
       .then(res => res.json())
       .then(json => {
         this.data = json;
+        this.responseLoaded = true;
+      })
+      .catch(error => {
+        this.statusMessage =
+          error.message === "timeout"
+            ? "Connection timed out."
+            : "Error fetching data.";
+        this.statusMessage += " Please try again later";
       });
   },
   data() {
     return {
-      data: []
+      data: [],
+      responseLoaded: false,
+      statusMessage: "Fetching Data..."
     };
   },
   computed: {
     filteredData() {
       const searchKeyRegex = new RegExp(this.searchKey, "i");
       return this.data.filter(item => searchKeyRegex.test(item.name));
+    }
+  },
+  methods: {
+    fetchWithTimeout(url) {
+      return new Promise((res, rej) => {
+        fetch(url).then(res, rej);
+        setTimeout(rej, 5000, new Error("timeout"));
+      });
     }
   }
 };
@@ -45,5 +65,13 @@ export default {
 .table {
   width: 90vw;
   margin: 0 auto;
+}
+
+.status {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
 }
 </style>
